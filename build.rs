9c96d976c04d41f649a05b679ad34258780a8cda
Nathan Jent <nathanjent@gmail.com>
2024-06-19 11:25:40 -0400

    Use palette image when mapping BMP to index data

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
-use std::env;
 use quote::quote;
+use std::collections::HashMap;
+use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
@@ -9,45 +10,41 @@ fn main() {
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable must be specified");
 
     if let Ok(opening_frame) = open("gfx/full-frame.png") {
-        let frame = opening_frame.into_rgb8();
+        if let Ok(palette_img) = open("palettes/shiny-16-1x.png") {
+            let palette_img = palette_img.into_rgb8();
+            let opening_frame = opening_frame.into_rgb8();
 
-        let img_data: Vec<u8> = frame.pixels().map(|p| {
-            let [r, g, b] = p.0;
-            match (r, g, b) {
-                (0, 0, 0) => 0x0,
-                (60, 47, 82) => 0x1,
-                (140, 127, 144) => 0x2,
-                (188, 176, 179) => 0x3,
-                (226, 226, 226) => 0x4,
-                (216, 104, 48) => 0x5,
-                (240, 149, 72) => 0x6,
-                (239, 208, 129) => 0x7,
-                (178, 39, 65) => 0x8,
-                (245, 70, 76) => 0x9,
-                (247, 156, 136) => 0xa,
-                (70, 86, 165) => 0xb,
-                (73, 149, 243) => 0xc,
-                (114, 222, 235) => 0xd,
-                (62, 197, 75) => 0xe,
-                (180, 230, 86) => 0xf,
-                _ => 0,
-            }
-        })
-        .collect();
+            let palette_map: HashMap<(u8, u8, u8), u8> = palette_img
+                .pixels()
+                .enumerate()
+                .map(|(i, p)| {
+                    assert!(i < 256);
+                    let [r, g, b] = p.0;
+                    ((r, g, b), i as u8)
+                })
+                .collect();
 
-        let img_width = frame.width();
-        let img_height = frame.height();
-        let frame_output = quote! {
-            pub const OPENING_IMG_WIDTH: u32 = #img_width;
-            pub const OPENING_IMG_HEIGHT: u32 = #img_height;
-            pub const OPENING_FRAME_IMG: &[u8] = &[#(#img_data),*];
-        };
-        
+            let img_data: Vec<u8> = opening_frame
+                .pixels()
+                .map(|p| {
+                    let [r, g, b] = p.0;
+                    *palette_map.get(&(r, g, b)).unwrap_or(&0u8)
+                })
+                .collect();
 
-        let frame_output_file = File::create(format!("{out_dir}/frame.rs"))
-            .expect("Failed to open frame.rs for writing");
-        let mut frame_writer = BufWriter::new(frame_output_file);
+            let img_width = opening_frame.width();
+            let img_height = opening_frame.height();
+            let frame_output = quote! {
+                pub const OPENING_IMG_WIDTH: u32 = #img_width;
+                pub const OPENING_IMG_HEIGHT: u32 = #img_height;
+                pub const OPENING_FRAME_IMG: &[u8] = &[#(#img_data),*];
+            };
 
-        write!(&mut frame_writer, "{frame_output}").unwrap();
+            let frame_output_file = File::create(format!("{out_dir}/frame.rs"))
+                .expect("Failed to open frame.rs for writing");
+            let mut frame_writer = BufWriter::new(frame_output_file);
+
+            write!(&mut frame_writer, "{frame_output}").unwrap();
+        }
     }
 }
